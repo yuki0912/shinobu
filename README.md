adc272bd58c6991e71699df30a876fe43f0c1759
yuki0912 <96724502+yuki0912@users.noreply.github.com>
2021-12-27 18:43:57 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# shinobu
+<h1 align="center">
+  <a href="https://github.com/yuki0912/shinobu"><img src="https://static.zerochan.net/Inuyose.Shinobu.full.3148762.jpg" avtar_c_icon"></a>
+  <br>
+  Inuyose Shinobu
+  <br>
+  <br>
+</h1>
+
+ ## Invite link
+
+[invite Shinobu bot](https://discord.com/api/oauth2/authorize?client_id=865614891193073695&permissions=8&scope=bot)
+    
+ [Join My Server](https://discord.gg/gmEc5WKrvR)
+   
+    
+
+## Shinobu
+This is my first coding 
+
+discord.js v12 / node v12.16.1
---
+++ index.js
@@ -0,0 +1,57 @@
+const { Collection, Client, Discord } = require('discord.js');
+const fs = require('fs');
+const client = new Client({
+    disableEveryone: true,
+    partials: ['MESSAGE', 'CHANNEL', 'REACTION']
+});
+const config = require('./config.json');
+const prefix = config.prefix;
+const token = config.token;
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync('./commands/');
+['command'].forEach(handler => {
+    require(`./handlers/${handler}`)(client);
+});
+client.on('ready', () => {
+    client.user.setPresence({
+        activity: {
+            name: `${client.guilds.cache.size} Server`,
+
+            // -----ALL TYPES----
+            // PLAYING
+            // WATCHING
+            // STREAMING
+            // LISTENING
+
+            type: 'PLAYING'
+        },
+
+        //TYPES
+        // -- dnd ( do not disturb )
+        // -- idle
+        // -- invisible
+        // -- online
+
+        status: 'online'
+    })
+    console.log(`${client.user.username} ✅`)
+});
+client.on('message', async message => {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+    if (!message.guild) return;
+    if (!message.member)
+        message.member = await message.guild.fetchMember(message);
+    const args = message.content
+        .slice(prefix.length)
+        .trim()
+        .split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    if (cmd.length == 0) return;
+    let command = client.commands.get(cmd);
+    if (!command) command = client.commands.get(client.aliases.get(cmd));
+    if (command) command.run(client, message, args);
+});
+
+client.login(token);
