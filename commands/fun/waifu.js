0ecea0f781bc9eb3521db87508701fec6955c70d
Yuki <96724502+yuki0912@users.noreply.github.com>
2021-12-28 22:44:46 +0800

    Update and rename commands/Fun/waifu.js to commands/fun/waifu.js

--- commands/fun/waifu.js
+++ commands/fun/waifu.js
@@ -2,9 +2,9 @@ const random = require('something-random-on-discord').Random
 const { MessageEmbed } = require('discord.js')
 
 module.exports = {
-    name: 's!waifu',
+    name: 'waifu',
     category: 'fun',
-    aliases: ['s!Waifu', 's!waifu'],
+    aliases: ['Waifu', 'waifu'],
     description: 'Get Fresh waifu Images :D',
     run: async (client, message, args) => {
         let data = await random.getAnimeImgURL('waifu')
