1c6726bffe723472c71bdebec4fecad453ac40fa
Yuki <96724502+yuki0912@users.noreply.github.com>
2022-12-08 12:47:24 +0800

    Update report.js

--- commands/info/report.js
+++ commands/info/report.js
@@ -1,27 +1,26 @@
 const { client, Message, MessageEmbed } = require("discord.js")
 
 module.exports = {
-    name: "report",
-    category: 'info',
-    /**
-    *@param {Client} client
-    *@param {Message} message
-    *@param {String[]} args
-    */
-    run: async (client, message, args) => {
-        const owner = client.users.cache.get();
+  name: "report",
+ /**
+  *@param {Client} client
+  *@param {Message} message
+  *@param {String[]} args
+  */
+  run: async (client, message, args) => {
+    const owner = client.users.cache.get(your user ID);
 
-        const query = args.join(" ");
-        if (!query) return message.reply('please specify a query');
+    const query = args.join(" ");
+    if (!query) return message.reply('please text 00');
 
-        const reportEmbed = new MessageEmbed()
-            .setTitle('NEW Bug')
-            .addField('Author', message.author.toString(), true)
-            .addField('Guild', message.guild.name, true)
-            .addField('Report', query)
-            .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
-            .setTimestamp();
+    const reportEmbed = new MessageEmbed()
+      .setTitle('NEW Bug!')
+      .addField('Author', message.author.toString(), true)
+      .addField('Guild', message.guild.name, true)
+      .addField('Report', query)
+      .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp();
 
-        owner.send(reportEmbed)
-    }
-}
+    owner.send(reportEmbed)
+  },
+};
